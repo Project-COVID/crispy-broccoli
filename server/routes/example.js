5b7bf07168852e98ba612e61ba902188e66aca7c
Mike <mike@blokur.com>
2020-03-21 19:07:16 +0000

    Add example request validation

--- server/routes/example.js
+++ server/routes/example.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const Joi = require('joi');
 
 const Response = require('../response');
 
 module.exports = function() {
     const router = express.Router();
-    router.route('/').get((req, res) => {
-        req.log.info('Hello, world!');
-        return Response.OK({ hello: 'world' }).send(res);
+    router.route('/').get(async (req, res) => {
+        try {
+            await Joi.validate(req.query, {
+                foo: Joi.string()
+                    .valid(['bar'])
+                    .required(),
+            });
+            // TODO call controller code here
+            req.log.info('Hello, world!');
+            return Response.OK({ hello: 'world' }).send(res);
+        } catch (err) {
+            if (err.isJoi) {
+                return Response.BadRequest(err.details).send(res);
+            }
+            req.log.error(err);
+            return Response.InternalServerError().send(res);
+        }
     });
 
     return router;
