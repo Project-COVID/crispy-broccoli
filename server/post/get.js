b9f01d7e7aaed8ff4ce118735773ab896900b171
Mike Christensen <mschristensen@users.noreply.github.com>
2020-03-26 21:56:56 +0000

    Feature/3n gk nqf r misc (#19)
    * Upgrade prettier
    
    * Add tags
    
    * Add max limit, require lat/lon & return total docs

--- server/post/get.js
+++ server/post/get.js
@@ -13,55 +13,48 @@ function kmToRadian(km) {
 }
 
 function sanitisePosts(posts) {
-    return posts.map((post) => _.pick(post, ['title', 'body', 'type', 'location', 'tags', 'name', 'email']));
+    return posts.map(post => _.pick(post, ['title', 'body', 'type', 'location', 'tags', 'name', 'email']));
 }
 
 async function getPosts(type, lat, lon, radius, cursor, limit) {
-    try {
-        if (lat && lon) {
-            return Post.find({
-                type,
-                verified: true,
-                status: constants.statuses.active,
-                location: {
-                    $geoWithin: {
-                        $center: [[lon, lat], kmToRadian(radius || DEFAULT_RADIUS_KM)],
-                    },
-                },
-                ...(cursor && { _id: { $gt: cursor } }),
-            })
-                .sort({
-                    createdAt: -1,
-                })
-                .limit(limit || DEFAULT_LIMIT);
-        }
-        return Post.find({
-            type,
-            verified: true,
-            status: constants.statuses.active,
-            ...(cursor && { _id: { $gt: cursor } }),
-        })
+    const query = {
+        type,
+        verified: true,
+        status: constants.statuses.active,
+        location: {
+            $geoWithin: {
+                $center: [[lon, lat], kmToRadian(radius || DEFAULT_RADIUS_KM)],
+            },
+        },
+    };
+    const pagination = {};
+    if (cursor) {
+        pagination._id = { $gt: cursor };
+    }
+    return {
+        total: await Post.count(query),
+        posts: await Post.find(Object.assign(query, pagination))
             .sort({
                 createdAt: -1,
             })
-            .limit(limit || DEFAULT_LIMIT);
-    } catch (err) {
-        throw err;
-    }
+            .limit(limit || DEFAULT_LIMIT),
+    };
 }
 
-module.exports = async function (req) {
+module.exports = async function(req) {
     try {
         await Joi.validate(req.query, {
-            type: Joi.string().valid(Object.values(constants.types)).required(),
-            lat: Joi.number(),
-            lon: Joi.number(),
+            type: Joi.string()
+                .valid(Object.values(constants.types))
+                .required(),
+            lat: Joi.number().required(),
+            lon: Joi.number().required(),
             cursor: Joi.string().hex(),
-            limit: Joi.number(),
+            limit: Joi.number().max(10),
             radius: Joi.number(), // in km
         });
 
-        let posts = await getPosts(
+        let result = await getPosts(
             req.query.type,
             req.query.lat,
             req.query.lon,
@@ -70,11 +63,11 @@ module.exports = async function (req) {
             req.query.limit,
         );
         let nextCursor = null;
-        if (posts && posts.length > 0) {
-            nextCursor = posts[posts.length - 1]._id;
+        if (result && result.posts && result.posts.length > 0) {
+            nextCursor = result.posts[result.posts.length - 1]._id;
         }
 
-        return Response.OK({ posts: sanitisePosts(posts), nextCursor });
+        return Response.OK({ posts: sanitisePosts(result.posts), total: result.total, nextCursor });
     } catch (err) {
         if (err.isJoi) {
             return Response.BadRequest(err.details);
