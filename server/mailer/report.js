51a8ff26e61fb7a899b85daeb0fc547119b27d87
Julian Laval <hello@julianlaval.com>
2020-03-28 21:09:47 +0000

    Finalised Sendgrid emails + work on homepage JS

--- server/mailer/report.js
+++ server/mailer/report.js
@@ -5,7 +5,10 @@ const templateId = 'd-f5d2ed9407f14cc29cdeb07f5f094c6f';
 module.exports = function (postId, message, teardownHash) {
     return mailer.send({
         to: 'yourfriends+report@kindnessproject.xyz',
-        from: 'yourfriends@kindnessproject.xyz',
+        from: {
+            name: 'The Kindness Project',
+            email: 'yourfriends@kindnessproject.xyz',
+        },
         templateId,
         dynamic_template_data: {
             postId,
--- server/mailer/verify.js
+++ server/mailer/verify.js
@@ -1,18 +1,23 @@
 const mailer = require('./index');
-const constants = require('../models/constants');
 
 const templateId = 'd-cf45e42acba34685a5866e0dfe54e79d';
 
 module.exports = function (postType, postId, verifyHash, name, email) {
     return mailer.send({
-        to: email,
-        from: 'yourfriends@kindnessproject.xyz',
+        to: {
+            name: name,
+            email: email,
+        },
+        from: {
+            name: 'The Kindness Project',
+            email: 'yourfriends@kindnessproject.xyz',
+        },
         templateId,
         dynamic_template_data: {
             postId,
             verifyHash,
             name,
-            isOffer: postType === constants.types.offer,
+            isOffer: postType === 'offer',
         },
     });
 };
