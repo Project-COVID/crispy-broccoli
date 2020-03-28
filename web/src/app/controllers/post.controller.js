ccb8ca0614e42efaa2971d4d7b86b10ffc2555e1
Mike <mike@blokur.com>
2020-03-28 16:06:21 +0000

    Add form validation and submission

--- web/src/app/controllers/post.controller.js
+++ web/src/app/controllers/post.controller.js
@@ -1,7 +1,62 @@
 'use strict';
 
-angular.module('app').controller('postController', function () {
+angular.module('app').controller('postController', function ($http, validationService, $stateParams) {
 
   var ctrl = this;
+
+  var schema = { // Fields visible in the UI that can display specific errors
+    name: {
+      required: true,
+      min: 1,
+      max: 20
+    },
+    email: {
+      required: true,
+      validate: 'email'
+    },
+    body: {
+      required: true,
+      max: 2000
+    }
+  };
+
+  ctrl.data = {};
+  ctrl.errors = {};
+  ctrl.display = {};
+
+  ctrl.validateField = function (field) {
+    ctrl.errors[field] = validationService.validateField(ctrl.data[field], schema[field]);
+  };
+
+  ctrl.submit = function () {
+
+    // Basic validation
+
+    var err;
+    _.forEach(_.keys(schema), function (field) {
+      ctrl.validateField(field);
+      if (ctrl.errors[field] !== undefined) {
+        err = true;
+      }
+    });
+
+    if (err) {
+      ctrl.errors.general = 'Please fix the errors below to continue';
+      validationService.scrollToError();
+      return;
+    }
+    
+    ctrl.display.isLoading = true;
+
+    $http.post(`/api/v1/post/${$stateParams.id}/reply`, ctrl.data).then(function (res) {
+      ctrl.display.successModalVisible = true;
+    }).catch(function (err) {
+      ctrl.errors = validationService.parseErrors(err.data, schema);
+      validationService.scrollToError();
+    }).finally(function () {
+      ctrl.display.isLoading = false;
+    });
+
+  };
 
 });
